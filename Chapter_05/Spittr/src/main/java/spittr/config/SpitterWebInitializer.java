f5fe60ed402d794ea36424f222e40fd417f2567f
wawu <wawu@Ctrip.com>
2017-10-11 21:22:17 +0800

    学习了SpitterWebInitializer类并为其添加了注释，便于理解

--- Chapter_05/Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
+++ Chapter_05/Spittr/src/main/java/spittr/config/SpitterWebInitializer.java
@@ -4,21 +4,39 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import spittr.web.WebConfig;
 
+/**
+ * 实现了WebApplicationInitializer接口的类将完成配置Servlet容器的任务
+ */
 public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-  
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class<?>[] { RootConfig.class };
-  }
 
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class<?>[] { WebConfig.class };
-  }
+    /**
+     * 该方法返回的类将用来定义ContextLoaderListener创建的应用上下文中的bean
+     * ContextLoaderListener加载应用中除了Web组件的bean之外的其他bean
+     * @return
+     */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
 
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] { "/" };
-  }
+    /**
+     * 该方法返回的类将用来定义DispatcherServlet应用上下文中的bean
+     * DispatcherServlet加载包含Web组件的bean
+     * @return
+     */
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    /**
+     * 该方法会将一根或多个路径映射到DispatcherServlet上
+     *
+     * @return
+     */
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 
 }
